2898ce556e3b27a6978f67e8e81e21069e7fa54e
Jatos Detking <sashko125@gmail.com>
2019-03-29 00:18:23 +0200

    <kon>dvnnsbdjvbsdvbsdmn

--- ConsoleApp19/Program.cs
+++ ConsoleApp19/Program.cs
@@ -9,40 +9,49 @@ namespace ConsoleApp19
 {
     class Program
     {
-        static string[] ISort(string[] inputArray)
+        static string[] cvetove(string[] inputArray)
         {
+            int r=0, b=0, w=0;
+            int s = 0;
+            string[] temp = new string[inputArray.Length];
             for (int i = 0; i < inputArray.Length - 1; i++)
             {
-                for (int j = i + 1; j > 0; j--)
+                if (String.Equals(inputArray[i],"red"))
                 {
-                    int a = -1, b = -1;
-                    if (String.Equals(inputArray[j - 1], "red"))
-                        a = 0;
-                    if (String.Equals(inputArray[j - 1], "blue"))
-                        a = 1;
-                    if (String.Equals(inputArray[j - 1], "white"))
-                        a = 2;
-                    if (String.Equals(inputArray[j], "red"))
-                       b = 0;
-                    if (String.Equals(inputArray[j], "blue"))
-                        b = 1;
-                    if (String.Equals(inputArray[j], "white"))
-                        b = 2;
-                    if (a > b)
-                    {
-                        string temp = inputArray[j - 1];
-                        inputArray[j - 1] = inputArray[j];
-                        inputArray[j] = temp;
-                    }
+                    r++;
                 }
-                
+                if (String.Equals(inputArray[i], "blue"))
+                {
+                    b++;
+                }
+                if (String.Equals(inputArray[i], "white"))
+                {
+                    w++;
+                }
             }
+            while (r != 0)
+            {
+                temp[s] = "red";
+                r--;
+                s++;
+            } while (b != 0)
+            {
+                temp[s] = "blue";
+                b--;
+                s++;
+            } while (w != 0)
+            {
+                temp[s] = "while";
+                w--;
+                s++;
+            }
+            Array.Copy(temp, inputArray, inputArray.Length);
             return inputArray;
         }
             static void Main(string[] args)
             {
             string[] test = { "red","blue", "white" ,"red", "white","red","red", "white","red","red", "blue","red" };
-            Console.WriteLine(ISort(test));
+            Console.WriteLine(cvetove(test));
             for (int i = 0; i < test.Length; i++)
             {
                 Console.WriteLine(test[i]);
