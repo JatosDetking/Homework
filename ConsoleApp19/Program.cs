cbcbb508e136be71bdb6fbc43d8a6b2d6c76c192
Jatos Detking <sashko125@gmail.com>
2019-04-18 19:28:32 +0300

    Final

--- ConsoleApp19/Program.cs
+++ ConsoleApp19/Program.cs
@@ -10,34 +10,47 @@ namespace ConsoleApp19
     { 
         static void Main(string[] args)
         {
-            string[] test = { "red", "blue", "white", "red", "white", "red", "blue", "red", "white", "red", "red", "blue", "red", "blue" };
+            string[] test = {"blue", "white", "white", "white", "white", "white", "white", "white", "white", "white", "white", "white", "white", "white", "white", "white", "white", "white", "white", "white", "white", "white", "white", "white", "white", "red", "white", "red", "red", "blue", "blue", "blue", "red", "red", "red", "red", "red", "blue", "blue", "blue", "red", "red", "red", "blue", "red", "white", "red", "white", "red","red", "red", "white", "red", "blue", "red", "blue" };
             int a = 0;
             int b = test.Length-1;
-            for (int i = 0; i < b; i++)
+            int c = 0;
+            for (int i = 0; i <b+1; i++)
             {
-                if (String.Equals(test[i], "red"))
+                c++;
+                if (String.Equals(test[a], "red"))
                 {
-                    string tempp = test[i];
-                    test[i] = test[a];
-                    test[a] = tempp;
                     a++;
                 }
-                else if (String.Equals(test[i], "white"))
+                if (String.Equals(test[b], "white"))
+                {
+                    b--;
+                }
+                if (String.Equals(test[i], "white"))
                 {
                     string temp = test[i];
                     test[i] = test[b];
                     test[b] = temp;
                     b--;
                 }
-                else if(String.Equals(test[i], "blue"))
+                if (String.Equals(test[i], "red"))
                 {
-                    continue;
+                        string tempp = test[i];
+                        test[i] = test[a];
+                        test[a] = tempp;
+                        a++;
+                }
+                if (String.Equals(test[i], "blue"))
+                {
+                        continue;
                 }
             }
             for (int i = 0; i < test.Length; i++)
             {
                 Console.WriteLine(test[i]);
             }
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
              Console.ReadLine();
         }
     }
