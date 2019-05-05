82bbfabc7166153cb9141a9f693fa0337fb4c6fd
Jatos Detking <sashko125@gmail.com>
2019-05-06 01:16:35 +0300

    finalllll

--- ConsoleApp19/Program.cs
+++ ConsoleApp19/Program.cs
@@ -8,54 +8,59 @@ namespace ConsoleApp19
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void swap(string[] input, int i, int j)
         {
-             string[] test = {"white", "white", "white", "white", "blue","blue", "white", "red", "white", "blue", "white", "white", "white" };
-               int a = 0;
-               int b = test.Length-1;
-               int c = 0;
-               for (int i = 0; i < b;i++)
-               { 
-                   c++;
-
-                if (String.Equals(test[i], "white"))
-                {
-                    string temp = test[i];
-                    test[i] = test[b];
-                    test[b] = temp;
-                    b--;
-                    if (i > 0)
-                    {
-                        i--;
-                    }
-                    if (String.Equals(test[i], "red"))
-                {
-                    string tempp = test[i];
-                    test[i] = test[a];
-                    test[a] = tempp;
-                    a++;
-                    if(i>0)
-                    {
-                    i--;
-                    }
-                }
-
-
-                }
-
-
-               }
-               for (int i = 0; i < test.Length; i++)
-               {
-                   Console.WriteLine(test[i]);
-               }
-;
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.WriteLine(c);
-            Console.ReadLine();
+            string temp = input[i];
+            input[i] = input[j];
+            input[j] = temp;
         }
-    }
-}
 
+        public static void color(string[] input)
+        {
+            int a = 0;
+            int b = 0;
+            int c = input.Length - 1;
+
+            while (a <= c)
+            {
+
+                if (String.Equals(input[a], "red"))
+                {
+                    swap(input, a, b);
+                    a++;
+                    b++;
+                }
+                else if (String.Equals(input[a], "blue"))
+                {
+
+                    a++;
+                }
+                else if (String.Equals(input[a], "white"))
+                {
+
+                    swap(input, a, c);
+                    c--;
+                }
+                else
+                {
+
+                    break;
+                }
+            }
+        }
+            public static void Main(string[] args)
+            {
+            string[] input = { "white", "white", "white", "white", "blue", "blue", "white", "red", "white", "blue", "white", "white", "white"};
+
+                color(input);
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    Console.WriteLine(input[i]);
+                }
+
+            }
+
+        }
+    } 
 
